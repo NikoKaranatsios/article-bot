834aa6cec84f0396d8897cbdb9244d04b893bf8a
Niko Karanatsios <karanatsios.n@gmail.com>
2023-03-30 19:51:39 +0200

    move to node

---
+++ index.js
@@ -0,0 +1,8 @@
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  organization: "org-EhRcnFN06RPOnNAkNz1JgZin",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines({});
